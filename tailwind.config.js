2b854e3246e30bfdba1c1a33d93f3871ad333aec
kevinnls <developer@kevinnlsamuel.com>
2025-05-03 03:23:02 +0530

    setup 11ty and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["!node_modules/**", "**/*.md", "**/*.liquid", "**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
