ab9a67687615469aec186752e51fa9187ea37b97
kevinnls <developer@kevinnlsamuel.com>
2025-05-27 23:59:58 +0530

    tw: basic media query + dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import colors from 'tailwindcss/colors';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["!node_modules/**", "**/*.md", "**/*.liquid", "**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+		colors: {
+			primary: colors.violet[500],
+			secondary: colors.amber[400],
+		},
+	},
   },
   plugins: [],
 }
